8df5916acd2a6c2636b44a71b2d53fbad638dffb
ceylan <ceylan.cansaran@gmail.com>
2019-11-30 19:29:00 +0300

    FormApp

---
+++ FormApp/FormApp.Data/ContactSendDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FormApp.Data
+{
+    public class ContactSendDto
+    {
+        [Required, MinLength(2), MaxLength(150)]
+        public string FullName { get; set; }
+
+        [Required, MinLength(6), MaxLength(345)]
+        public string Email { get; set; }
+
+        [Required, MinLength(2), MaxLength(1024)]
+        public string Messsage { get; set; }
+    }
+}
---
+++ FormApp/FormApp/Controllers/ContactController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Send([FromBody]FormApp.Data.ContactSendDto contactSendDto)
+        {
+            if (ModelState.IsValid)
+            {
+                return BadRequest(error: "Hayırdır");
+            }
+            return new JsonResult("ok");
+        }
+    }
+}
