82f93b80fb2c387ece42718cfa997069a2a97bfb
ceylan <ceylan.cansaran@gmail.com>
2019-12-01 19:41:09 +0300

    01.12.2019

---
+++ GuestBook/GuestBook.Data/Context/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using GuestBook.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuestBook.Data.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+        { }
+
+        public DbSet<GuestNote> GuestNotes { get; set; }
+    }
+}
---
+++ GuestBook/GuestBook/Controllers/GuestBookController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GuestBook.Data.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuestBook.Controllers
+{
+    public class GuestBookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SendAction([FromBody]GuestBookSendActionDto guestBookSendActionDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("bizde yaptık ama yemezler");
+            }
+            return new JsonResult("ok");
+        }
+    }
+}
